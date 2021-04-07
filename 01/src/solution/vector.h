a59d900ce0f81b4a053a47d0790d25cc1d4ace6c
KapitanD <ddden97@gmail.com>
2021-04-07 17:00:53 +0300

    hw1

---
+++ 01/src/solution/vector.h
@@ -0,0 +1,16 @@
+#ifndef MY_VECTOR_INT_H
+#define MY_VECTOR_INT_H
+#include <stdlib.h>
+
+struct _VectorInt {
+    size_t size, capacity;
+    int* data;
+};
+
+#define VectorInt struct _VectorInt
+
+VectorInt* VectorInt_make_vector();
+void VectorInt_push_back(VectorInt* v, int a);
+void VectorInt_sort(VectorInt* v);
+VectorInt* VectorInt_merge_vectors(VectorInt** vs, unsigned int size);
+#endif
