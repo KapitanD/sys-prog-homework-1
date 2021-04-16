5f8934861cdf3dff2605d6b69f807fdbc08d103c
KapitanD <ddden97@gmail.com>
2021-04-16 10:13:26 +0300

    refactor: add docstrings and comments for main

--- 02/src/main.c
+++ 02/src/main.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 /**
  * @brief Count numer of commands in array, ending with NULL
  * @param cmds Array of commands, ending with NULL
@@ -20,6 +21,11 @@ size_t count_cmds(cmd** cmds) {
     }
 }
 
+
+/**
+ * @brief Count number of pipes in cmds array
+ * @param cmds Array of commands, ending with NULL
+ */
 size_t count_pipes(cmd** cmds) {
     size_t i = 0, count = 0;
     while(1) {
@@ -34,20 +40,23 @@ size_t count_pipes(cmd** cmds) {
 }
 
 int main(int argc, char** argv) {
-    int w_stat, prev_w_stat;
+    int w_stat;
     while(1) {
-        // printf("> ");
+        // Read comand or several comands from stdin
         cmd** cmds = read_command(stdin);
+        // i - current command index
         size_t i = 0;
         size_t cmds_count = count_cmds(cmds);
 
         if (cmds_count == 0) {
             break;
         }
+
+        // Processing # comments
         if (cmds_count != 0 && strcmp(cmds[0] -> name, "#") == 0) {
             continue;
         }
-
+        // Count number of pipes and create a pipe for every
         size_t pipes_count = count_pipes(cmds);
         int *pipefds = (int*)malloc(2*pipes_count*sizeof(int));
         for (size_t j = 0; j < pipes_count; ++j) {
@@ -56,13 +65,16 @@ int main(int argc, char** argv) {
                 exit(1);
             }
         }
+        // w_stat - status code of last command, init is -1
         w_stat = -1;
-        prev_w_stat = -1;
-        while(1) {
+        while(1) { // Processing commands
+            // init fds for child input and output is standart
             int child_input = 0, child_output = 1;
+            // cmds end with NULL
             if (cmds[i] == NULL) {
                 break;
             }
+            // skip command sep symbols
             if (strcmp(cmds[i] -> name, "|") == 0 
                 || strcmp(cmds[i] -> name, ">") == 0 
                 || strcmp(cmds[i] -> name, ">>") == 0 
@@ -71,14 +83,15 @@ int main(int argc, char** argv) {
                 i++;
                 continue;
             }
-
+            // if next cmd is |, redirect output
             if (i + 2 < cmds_count && strcmp(cmds[i + 1] -> name, "|") == 0) {
                 child_output = pipefds[i + 1];
             } 
+            // if prev cmd is |, redirect input
             if (i != 0 && strcmp(cmds[i - 1] -> name, "|") == 0) {
                 child_input = pipefds[i - 2];
             }
-
+            // process cd command
             if (strcmp(cmds[i] -> name, "cd") == 0) {
                 int err = chdir(cmds[i] -> argv[1]);
                 if (err != 0) {
@@ -159,14 +172,13 @@ int main(int argc, char** argv) {
                                     (char* const*) cmds[i] -> argv);
                 } else {
                     // Parent process
-                    if (prev_w_stat == 0) {
+                    if (w_stat == 0) {
                         i += 2;
                         continue;
                     }
                     wait(&w_stat);
                     if (w_stat != 0) {
                         i += 3;
-                        prev_w_stat = w_stat;
                         continue;
                     }
                 }
@@ -180,14 +192,13 @@ int main(int argc, char** argv) {
                                     (char* const*) cmds[i] -> argv);
                 } else {
                     // Parent process
-                    if (prev_w_stat > 0) {
+                    if (w_stat > 0) {
                         i += 2;
                         continue;
                     }
                     wait(&w_stat);
                     if (w_stat == 0) {
                         i += 3;
-                        prev_w_stat = w_stat;
                         continue;
                     }
                 }
@@ -222,7 +233,7 @@ int main(int argc, char** argv) {
                         close(child_input);
                     if (child_output != 1)
                         close(child_output);
-                    while(wait(&w_stat) >=0);
+                    while(wait(&w_stat) >= 0);
                 }
             }
             i++;
